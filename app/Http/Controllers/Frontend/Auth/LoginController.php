1aae9dec296d443b5ed09f20c7bc856124a4587a
Lupacescu Eduard <lupacescueduard@yahoo.com>
2017-07-06 18:33:11 +0300

    Implement user login.

--- app/Http/Controllers/Frontend/Auth/LoginController.php
+++ app/Http/Controllers/Frontend/Auth/LoginController.php
@@ -50,10 +50,27 @@ class LoginController extends Controller
      */
     protected function authenticated(Request $request, $user)
     {
+        $photo = $request->file('photo_path');
+
+        if ($photo && $user) {
+
+            $img_name = $request->file('photo_path')->getClientOriginalName();
+            $request->file('photo_path')->move(public_path('tmp'), $img_name);
+            $physicPhoto = public_path('uploads/' . $img_name);
+            $bytePhoto = $this->dHash($physicPhoto);
+            if ( ! $this->isValidPhoto($physicPhoto, $user)) {
+                access()->logout();
+                return redirect($this->redirectPath())->withFlashWarning('Aceasta poza nu coincide cu acest utilizator');
+            };
+        } else {
+            access()->logout();
+            return redirect($this->redirectPath())->withFlashWarning('Aceasta poza nu coincide cu acest utilizator');
+        }
+
         /*
          * Check to see if the users account is confirmed and active
          */
-        if (! $user->isConfirmed()) {
+        if ( ! $user->isConfirmed()) {
             access()->logout();
 
             // If the user is pending (account approval is on)
@@ -63,7 +80,7 @@ class LoginController extends Controller
 
             // Otherwise see if they want to resent the confirmation e-mail
             throw new GeneralException(trans('exceptions.frontend.auth.confirmation.resend', ['user_id' => $user->id]));
-        } elseif (! $user->isActive()) {
+        } elseif ( ! $user->isActive()) {
             access()->logout();
             throw new GeneralException(trans('exceptions.frontend.auth.deactivated'));
         }
@@ -124,7 +141,7 @@ class LoginController extends Controller
     public function logoutAs()
     {
         //If for some reason route is getting hit without someone already logged in
-        if (! access()->user()) {
+        if ( ! access()->user()) {
             return redirect()->route('frontend.auth.login');
         }
 
@@ -148,5 +165,65 @@ class LoginController extends Controller
 
             return redirect()->route('frontend.auth.login');
         }
+    }
+
+    public function dHash($file)
+    {
+        $hash = '';
+        $size = 8;
+        list($w, $h, $t) = getimagesize($file);
+        $im = imagecreatetruecolor($size + 1, $size);
+        imagefilter($im, IMG_FILTER_GRAYSCALE);
+        switch ($t) {
+            case 1:
+                $oi = imagecreatefromgif($file);
+                break;
+            case 2:
+                $oi = imagecreatefromjpeg($file);
+                break;
+            case 3:
+                $oi = imagecreatefrompng($file);
+                break;
+        }
+        imagecopyresampled($im, $oi, 0, 0, 0, 0, $size + 1, $size, $w, $h);
+        imagedestroy($oi);
+        for ($y = 0; $y < $size; $y++) {
+            $val = '';
+            for ($x = 0; $x < $size; $x++) {
+                $curr = imagecolorat($im, $x, $y);
+                $next = imagecolorat($im, $x + 1, $y);
+                $val .= ($curr > $next) ? 1 : 0;
+            }
+            $hash .= str_pad(dechex(bindec($val)), 2, 0, STR_PAD_LEFT);
+        }
+        imagedestroy($im);
+        return base_convert($hash, 16, 2);
+    }
+
+    public function HammingDistance($bin1, $bin2)
+    {
+        $a1 = str_split($bin1);
+        $a2 = str_split($bin2);
+        $count = count($a1) > count($a2) ? count($a2) : count($a1);
+        $dh = 0;
+        for ($i = 0; $i < $count; $i++)
+            if ($a1[$i] != $a2[$i]) $dh++;
+        return $dh;
+    }
+
+    public function isValidPhoto($path, $user)
+    {
+        $isValid = true;
+        $currentPhotoByte = $this->dHash($path);
+        if ($user->byte_photo != null) {
+            $user_photo_byte = $user->byte_photo;
+            $percentage = (strlen($user_photo_byte) - $this->HammingDistance($user_photo_byte, $currentPhotoByte)) * 100 / strlen($user_photo_byte);
+
+            if ($percentage > 70) {
+                $isValid = false;
+            }
+        }
+
+        return $isValid;
     }
 }
--- resources/views/frontend/auth/login.blade.php
+++ resources/views/frontend/auth/login.blade.php
@@ -13,21 +13,34 @@
 
                 <div class="panel-body">
 
-                    {{ Form::open(['route' => 'frontend.auth.login.post', 'class' => 'form-horizontal']) }}
+                    {{ Form::open(['route' => 'frontend.auth.login.post', 'class' => 'form-horizontal', "enctype"=> "multipart/form-data"]) }}
 
                     <div class="form-group">
                         {{ Form::label('email', trans('validation.attributes.frontend.email'), ['class' => 'col-md-4 control-label']) }}
                         <div class="col-md-6">
-                            {{ Form::email('email', null, ['class' => 'form-control', 'maxlength' => '191', 'required' => 'required', 'autofocus' => 'autofocus', 'placeholder' => trans('validation.attributes.frontend.email')]) }}
+                            {{ Form::email('email', null,
+                            ['class' => 'form-control', 'maxlength' => '191', 'required' => false, 'autofocus' => 'autofocus', 'placeholder' => trans('validation.attributes.frontend.email')]) }}
                         </div><!--col-md-6-->
                     </div><!--form-group-->
 
                     <div class="form-group">
                         {{ Form::label('password', trans('validation.attributes.frontend.password'), ['class' => 'col-md-4 control-label']) }}
                         <div class="col-md-6">
-                            {{ Form::password('password', ['class' => 'form-control', 'required' => 'required', 'placeholder' => trans('validation.attributes.frontend.password')]) }}
+                            {{ Form::password('password', ['class' => 'form-control', 'required' => false, 'placeholder' => trans('validation.attributes.frontend.password')]) }}
                         </div><!--col-md-6-->
                     </div><!--form-group-->
+                    <div class="form-group">
+                        <br>
+                        <div class="col-md-8">
+                            <h2>Sau incarcati o poza</h2>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        {{ Form::label('password_confirmation', trans('validation.attributes.frontend.photo'), ['class' => 'col-md-4 control-label']) }}
+                        <div class="col-md-6">
+                            {{ Form::file('photo_path') }}
+                        </div>
+                    </div>
 
                     <div class="form-group">
                         <div class="col-md-6 col-md-offset-4">
