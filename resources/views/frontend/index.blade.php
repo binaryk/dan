c4e89e768a35515193669f49e6b549070e66d381
Lupacescu Eduard <lupacescueduard@yahoo.com>
2017-07-16 18:07:15 +0300

    CurrentWork

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -11,7 +11,26 @@
                 </div>
 
                 <div class="panel-body">
-                    
+
+                    Acesta este proiectul de licenta, in care vom demonstra cum functioneaza algoritmul <b>Hamming</b> pentru recunoastere de imagini.
+
+                    <hr>
+                    <p>
+                        Pasii pentru a realiza inregistrarea unui utilizator sunt:
+                    </p>
+                    <ol>
+                        <li>Completarea datelor de utilizator (Prenume/Nume/Email/Parola si Confirmare)</li>
+                        <li>Capturarea unei poze cu ajutorul camerei web</li>
+                        <li>Transmiterea datelor spre procesare la server</li>
+                        <li>Convertirea imaginii din format <i>base64</i> in jpeg</li>
+                        <li>Stocarea imaginii pe disk (HDD)</li>
+                        <li>Convertirea imaginii in biti, cu ajutorul algoritmului <b>dHash</b></li>
+                        <li>Verificarea validitatii imaginii in lista de useri, cu ajutorul algoritmului <b>Hamming</b>, procesat pe biti</li>
+                        <li>Daca nici un alt user nu are o poza similara mai mult de 70% cu poza incarcata de utilizatorul curent, se lasa pasul urmator: </li>
+                        <li>Stocarea datelor de utilizator precum si calea spre imagine, dar si formatul ei binar, ca o inregistrare noua in tabela de <i>users</i></li>
+                        <li>Daca nu se trece de validare, procesarea se opreste aici.</li>
+                    </ol>
+
                 </div>
             </div><!-- panel -->
 
